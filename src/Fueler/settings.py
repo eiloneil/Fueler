69f42620055c79f3fde4dfbe3b418652a5cd5a9b
Eilon Eilstein <eiloneil@fb.com>
2023-04-29 17:09:05 +0300

    add delete-row btn

--- src/Fueler/settings.py
+++ src/Fueler/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # manually-added app
     "personal_dir",
-
+    # 'datepicker',
 
     # auto-generated
     "django.contrib.admin",
--- src/personal_dir/views.py
+++ src/personal_dir/views.py
@@ -3,10 +3,10 @@ from django.http import HttpResponse
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
-from .forms import TextInputForm, INPUTS
+from .forms import TextInputForm, INPUTS, DateRangeForm
 import time
 from datetime import datetime
-from .fuel_utils import calc_fuel_metrics, get_db_from_firebase
+from .fuel_utils import calc_fuel_metrics, get_db_from_firebase, delete_rows_within_range, push_to_db
 
 TEST = True
 # TEST = False
@@ -30,23 +30,25 @@ def home_page_view(req):
 def btn_add_row(request):
     print('btn_add_row CLICKED')
 
-    # Get data from the database
-    # data = get_db_from_firebase()
-
+    btn_context = {'btn_txt': "Submit", 'switch_action': {
+        'btn_name': 'Delete previous data', 'url': 'delete_row'}}
     if request.method == 'POST':
         form = TextInputForm(request.POST)
         if form.is_valid():
 
             input_data = {ip: form.cleaned_data[ip] for ip in INPUTS}
-            print('@@@  @@@')
             # Do something with the text_input value, such as store it as a Python variable
-            new_data = calc_fuel_metrics(input_data)
-            return render(request, 'add_new_row.html', {**input_data})
+            new_data = calc_fuel_metrics(input_data, request)
+            if new_data is None:
+                pass
+            else:
+                push_to_db(new_data, 'fuel_raw')
+                btn_context.update({'is_submitted_successfully':input_data['date']})
     else:
 
         form = TextInputForm(initial=init_text)
-    return render(request, 'add_new_row.html', {'form': form})
-    print('btn_add_row CLICKED')
+
+    return render(request, 'add_new_row.html', {'form': form, **btn_context, })
 
 
 def btn_show_raw_data(request):
@@ -57,7 +59,26 @@ def btn_show_raw_data(request):
 
     # Pass data as context data to the template
     context = {'data': data}
-    print(data)
 
     # Use the data in your Django view or model as needed
     return render(request, 'raw_data.html', context)
+
+
+def btn_delete_row(request):
+
+    btn_context = {'btn_txt': "Delete",
+                   'switch_action': {'btn_name': 'Add another row', 'url': 'add_row'}, }
+    if request.method == 'POST':
+        form = DateRangeForm(request.POST)
+        if form.is_valid():
+            start_date = form.cleaned_data['start_date']
+            end_date = form.cleaned_data['end_date']
+            delete_rows_within_range(start_date, end_date, db_name='fuel_raw')
+    else:
+        form = DateRangeForm(initial={'start_date': today, 'end_date': today})
+    context = {
+        'form': form,
+        **btn_context,
+
+    }
+    return render(request, 'remove_dates.html', context)
